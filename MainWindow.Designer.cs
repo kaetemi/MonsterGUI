b9ae9eec2a15209c7a88e8b412bf6d1b1c79202a
kaetemi <kaetemi@gmail.com>
2015-06-14 13:09:11 +0200

    Layout

--- MainWindow.Designer.cs
+++ MainWindow.Designer.cs
@@ -409,6 +409,8 @@
 			// 
 			// groupBox9
 			// 
+			this.groupBox9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
 			this.groupBox9.Controls.Add(this.coolDownIntf);
 			this.groupBox9.Controls.Add(this.supportAbilitiesCheck);
 			this.groupBox9.Controls.Add(this.metalDetectorIntf);
@@ -574,8 +576,6 @@
 			// 
 			// groupBox7
 			// 
-			this.groupBox7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.groupBox7.Controls.Add(this.label17);
 			this.groupBox7.Controls.Add(this.label16);
 			this.groupBox7.Controls.Add(this.label15);
